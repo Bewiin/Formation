e44a99cb85b8f1679a32c748c94ee99be84aace3
Bewiin <steven.lavaure@hotmail.fr>
2023-10-24 15:56:37 +0200

    travail sur le header

---
+++ 3_Mon_Projet_Fil_Rouge/script.js
@@ -0,0 +1,19 @@
+const menu = document.getElementById("menu");
+menu.style.backgroundColor = "red";
+menu.style.border = "red";
+
+
+
+
+
+/* Modifier les variables bootstrap
+//récupérer la valeur d'une variable CSS
+function getCssVariable(attribut){
+    const rootAttr = getComputedStyle(root)
+    return rootAttr.getPropertyValue(attribut);
+}
+//fonction pour modifier la valeur d'une variable
+function setCssValue(attribut, value){
+    root.style.setProperty(attribut, value);
+}
+setCssValue('--bg', 'yellow');*/
